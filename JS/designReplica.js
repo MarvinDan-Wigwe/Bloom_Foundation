1aad91cf7d8ca1717e28d48eeb132d8905e5bdc9
Marvin Dan-Wigwe <marvindanielkay@gmail.com>
2025-10-18 14:45:59 +0100

    added extra image areas and an about page to be designed

---
+++ JS/designReplica.js
@@ -0,0 +1,10 @@
+alert("WELCOME TO BI_ASCEND");
+
+let username = prompt ("Select a username");
+let password = prompt ("Enter a numeric password");
+
+if (isNaN(password)) {
+    alert("Incorrect Character Selection")
+} else {
+    alert("Enjoy your time")
+}
